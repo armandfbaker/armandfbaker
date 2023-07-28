af986385e57035704de42148c8125970e5efac36
armandfbaker <140722775+armandfbaker@users.noreply.github.com>
2023-07-28 08:40:19 -0400

    Replacing with old files
    moving in all of the old files to see what happens

---
+++ includes/navbar.php
@@ -0,0 +1,11 @@
+<div id="navcontainer">
+<div align="center">
+<a href="index.html" id="current">Home</a> &nbsp;&nbsp; &nbsp;&nbsp;
+<a href="publications.html">Publications</a> &nbsp;&nbsp;&nbsp;&nbsp;
+<a href="lectures.html">Lectures</a> &nbsp;&nbsp;&nbsp;&nbsp;
+<a href="poems.html">Machado/Poems</a> &nbsp;&nbsp;&nbsp;&nbsp;
+<a href="cv.html">Curriculum Vitae</a> &nbsp;&nbsp;&nbsp;&nbsp;
+<a href="fiction.html">Fiction</a> &nbsp;&nbsp;&nbsp;&nbsp;
+<a href="contact.html">Contact</a>
+</div>
+</div>
